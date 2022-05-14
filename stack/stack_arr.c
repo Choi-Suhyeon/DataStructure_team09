ee7918534925ea461aa809a301e4682917bc5a12
lavegas1 <101579984+lavegas1@users.noreply.github.com>
2022-05-14 21:57:59 +0900

    Update stack_arr.c

--- stack/stack_arr.c
+++ stack/stack_arr.c
@@ -1,39 +1,83 @@
-#include <stdio.h>
-#define MAX_STACK_SIZE 100
+#ifndef DATA_STRUCTURE_STACK_H
+#define DATA_STRUCTURE_STACK_H
 
-int stack[MAX_STACK_SIZE];
-int top = -1;
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
 
-int isFull() {
-	if (top >= MAX_STACK_SIZE - 1)
-		return 1;
-	else
-		return 0;
-}
+#define DEFINE_STACK(T) \
+   typedef struct _##T##_StackNode *  T##_StPtr; \
+   typedef struct _##T##_StackNode { \
+      T           data;               \
+      T##_StPtr next;               \
+   } T##_StackNode;                 \
+   typedef struct {                  \
+        T##_StackNode *    top;    \
+        unsigned long long length; \
+    } T##_Stack;                    \
+    \
+    T##_Stack * T##Stack() {                           \
+        T##_Stack * st = calloc(1, sizeof(T##_Stack)); \
+        return st;                                     \
+    }                                                  \
+    \
+    void T##Push(T##_Stack * const st, T value) {             \
+        assert(st->length < ULLONG_MAX);                      \
+                                                              \
+        T##_StackNode * node = malloc(sizeof(T##_StackNode)); \
+        node->data = value;                                   \
+       node->next = st->top;                                 \
+       st->top = (T##_StPtr)node;                            \
+        ++st->length;                                         \
+    }                                                         \
+    \
+    T T##Pop(T##_Stack * const st) {    \
+        assert(st->length);             \
+                                        \
+        T##_StackNode * temp = st->top; \
+      T result = temp->data;          \
+      st->top = temp->next;           \
+      free(temp);                     \
+        --st->length;                   \
+                                        \
+      return result;                  \
+   }                                   \
+    \
+    void T##DeleteStack(T##_Stack * const st) {          \
+        while (st->length) {                             \
+            T##_StackNode * next_node = st->top->next;   \
+                                                         \
+            free(st->top);                               \
+            st->top = next_node;                         \
+            --st->length;                                \
+        }                                                \
+                                                         \
+        free(st);                                        \
+    }                                                    \
+    \
+    void T##TestForNormalcySt(T##_Stack * const st) { \
+        const unsigned kLenBool  = !!st->length,      \
+                       kTopBool  = !!st->top;         \
+                                                      \
+        assert(!(kLenBool | kTopBool)                 \
+             || (kLenBool & kTopBool));               \
+    }                                                 \
+    \
+    unsigned T##IsEmptyStack(T##_Stack * const st) { \
+        T##TestForNormalcySt(st);                    \
+        return !st->length;                          \
+    }                                                \
+    \
+    unsigned T##IsFullStack(T##_Stack * const st) { \
+        T##TestForNormalcySt(st);                   \
+        return st->length == ULLONG_MAX;            \
+    }
+#define STACK(T)          T##_Stack *
+#define NEW_STACK(T)      T##Stack()
+#define PUSH(T)          T##Push
+#define POP(T)           T##Pop
+#define DELETE_STACK(T)   T##DeleteStack
+#define IS_EMPTY_STACK(T) T##IsEmptyStack
+#define IS_FULL_STACK(T)  T##IsFullStack
 
-int isEmpty() {
-	if (top == -1)
-		return 1;
-	else
-		return 0;
-}
-
-void push(int value) {
-	if (isFull()) {
-		printf("Stack is full\n");
-		return;
-	}
-		stack[++top] = value;
-}
-
-int pop() {
-	if (isEmpty()) {
-		printf("Stack is empty\n");
-		return;
-	}
-	return stack[top--];
-}
-
-int main() {
-	return 0;
-}
+#endif
