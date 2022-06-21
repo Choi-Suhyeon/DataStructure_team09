43360722a5e0d3a2762e49459bdf8c93f11e96c0
yuu4172 <101076205+yuu4172@users.noreply.github.com>
2022-06-21 12:35:20 +0900

    셸 정렬

---
+++ ShellSort.c
@@ -0,0 +1,14 @@
+#include "Swap.h"
+#include "BubbleSort.h"
+
+void ShellSort(void* seq, const unsigned long long type_size, const unsigned long long length, int (*comparator)(const void* x, const void* y)) {
+    byte* list = (byte*)seq;
+    for (int gap = length / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < length; i += gap) {
+            for (int j = i; j > 0; j -= gap) {
+                if (comparator(list + ((j - 1) * type_size), list + (j * type_size)) > 0)
+                    swap(list + ((j - 1) * type_size), list + (j * type_size), type_size);
+            }
+        }
+    }
+}
