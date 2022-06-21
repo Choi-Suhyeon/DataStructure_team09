a31e1843b291fbdc95eb04e50ef7e233a590caa2
CHOI SUHYEON <1AzyF0x@protonmail.ch>
2022-06-21 15:06:12 +0900

    modify countingSort.h correctly

--- countingSort.h
+++ countingSort.h
@@ -4,6 +4,9 @@
 #include "predef.h"
 #include <stdio.h>
 
-void countingSort(int *, uLLong);
+void uIntCountingSort(unsigned *, uLLong);
+void uCharCountingSort(unsigned char *, uLLong);
+void uShortCountingSort(unsigned short *, uLLong);
+void uLLongCountingSort(uLLong *, uLLong);
 
 #endif //DATA_STRUCTURE_COUNTINGSORT_H
